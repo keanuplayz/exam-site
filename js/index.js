55e663f1b51e74f2d3cb158ebb92177e4fa606e5
Keanu <keanutimmermans8@gmail.com>
2019-11-20 14:15:05 +0100

    Attempt to fix tabs.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 var tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
 var contentEls = document.querySelectorAll('.content');
 
-tabBar.listen('MDCTabBar:activated', function (event) {
-    document.querySelector('.content--active').classList.remove('content--active');
-
-    contentEls[event.detail.index].classList.add('content--active');
+tabBar.listen('MDCTabBar:activated', function(event) {
+  // Hide currently-active content
+  document.querySelector('.content--active').classList.remove('content--active');
+  // Show content for newly-activated tab
+  contentEls[event.detail.index].classList.add('content--active');
 });
